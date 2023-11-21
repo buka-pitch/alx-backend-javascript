060b4ca4fdd97a3f15b18fae8d909e52a3255f70
Buka <birukyonas331@gmail.com>
2023-11-21 21:31:07 +0000

    node

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,16 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable',function (){
-    let chunk = process.stdin.read();
-    if (chunk !== null ){
-        process.stdout.write(chunk);
-    }
-})
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
